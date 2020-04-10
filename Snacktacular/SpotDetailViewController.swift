6125c78bb90d03365c45ad43ead20397f476ace1
schmitck <schmitck@bc.edu>
2020-04-10 19:02:09 -0400

    Finished 9.1, 9.2, 9.3 and 9.4

--- Snacktacular/SpotDetailViewController.swift
+++ Snacktacular/SpotDetailViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import GooglePlaces
 
 class SpotDetailViewController: UIViewController {
     
@@ -16,16 +17,38 @@ class SpotDetailViewController: UIViewController {
     @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var tableView: UITableView!
 
+  var spot: Spot!
+  
     override func viewDidLoad() {
         super.viewDidLoad()
+      if spot == nil {
+        spot = Spot()
+      }
+      
+      nameField.text = spot.name
+      addressField.text = spot.address
+      
     }
 
+  func updateUserInterface() {
+    nameField.text = spot.name
+    addressField.text = spot.address
+    
+  }
+  
     @IBAction func photoButtonPressed(_ sender: UIButton) {
     }
     
     @IBAction func reviewButtonPressed(_ sender: UIButton) {
     }
     
+  
+  @IBAction func lookupPlacePressed(_ sender: UIBarButtonItem) {
+    let autocompleteController = GMSAutocompleteViewController()
+    autocompleteController.delegate = self
+    present(autocompleteController, animated: true, completion: nil)
+  }
+  
     @IBAction func saveButtonPressed(_ sender: UIBarButtonItem) {
     }
     
@@ -37,4 +60,39 @@ class SpotDetailViewController: UIViewController {
             navigationController?.popViewController(animated: true)
         }
     }
+}
+
+extension SpotDetailViewController: GMSAutocompleteViewControllerDelegate {
+
+  // Handle the user's selection.
+  func viewController(_ viewController: GMSAutocompleteViewController, didAutocompleteWith place: GMSPlace) {
+    print("Place name: \(place.name)")
+    print("Place ID: \(place.placeID)")
+    print("Place attributions: \(place.attributions)")
+    spot.name = place.name ?? ""
+    spot.address = place.formattedAddress ?? ""
+    spot.coordinate = place.coordinate
+    dismiss(animated: true, completion: nil)
+    updateUserInterface()
+  }
+
+  func viewController(_ viewController: GMSAutocompleteViewController, didFailAutocompleteWithError error: Error) {
+    // TODO: handle the error.
+    print("Error: ", error.localizedDescription)
+  }
+
+  // User canceled the operation.
+  func wasCancelled(_ viewController: GMSAutocompleteViewController) {
+    dismiss(animated: true, completion: nil)
+  }
+
+  // Turn the network activity indicator on and off again.
+  func didRequestAutocompletePredictions(_ viewController: GMSAutocompleteViewController) {
+    UIApplication.shared.isNetworkActivityIndicatorVisible = true
+  }
+
+  func didUpdateAutocompletePredictions(_ viewController: GMSAutocompleteViewController) {
+    UIApplication.shared.isNetworkActivityIndicatorVisible = false
+  }
+
 }
--- Snacktacular/SpotsListViewController.swift
+++ Snacktacular/SpotsListViewController.swift
@@ -8,12 +8,130 @@
 
 import UIKit
 import CoreLocation
+import Firebase
+import FirebaseUI
+import GoogleSignIn
 
 class SpotsListViewController: UIViewController {
+  
+  @IBOutlet weak var tableView: UITableView!
+  var spots: Spots!
+  var authUI: FUIAuth!
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    authUI = FUIAuth.defaultAuthUI()
+    // You need to adopt a FUIAuthDelegate protocol to receive callback
+    authUI.delegate = self
+    spots = Spots()
+    tableView.delegate = self
+    tableView.dataSource = self
+    tableView.isHidden = true
     
-    @IBOutlet weak var tableView: UITableView!
+    spots.spotArray.append(Spot(name: "El Pelon", address: "Comm Ave", coordinate: CLLocationCoordinate2D(), averageRating: 0.0, numberOfReviews: 0, postingUserID: "", documentID: ""))
+    spots.spotArray.append(Spot(name: "Dunkin", address: "Comm Ave", coordinate: CLLocationCoordinate2D(), averageRating: 0.0, numberOfReviews: 0, postingUserID: "", documentID: ""))
+    spots.spotArray.append(Spot(name: "Shake Shack", address: "The Street - Chestnut Hill", coordinate: CLLocationCoordinate2D(), averageRating: 0.0, numberOfReviews: 0, postingUserID: "", documentID: ""))
+  }
+  
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
+    signIn()
+  }
+  
+  func signIn() {
+    let providers: [FUIAuthProvider] = [
+      FUIGoogleAuth(), FUIEmailAuth(),
+    ]
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    if authUI.auth?.currentUser == nil {
+      self.authUI.providers = providers
+      let loginViewController = authUI.authViewController()
+      loginViewController.modalPresentationStyle = .fullScreen
+      present(loginViewController, animated: true, completion: nil)
+    } else {
+      tableView.isHidden = false
     }
+  }
+  
+  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    if segue.identifier == "ShowSpot" {
+      let destination = segue.destination as! SpotDetailViewController
+      let selectedIndexPath = tableView.indexPathForSelectedRow!
+      destination.spot = spots.spotArray[selectedIndexPath.row]
+    } else {
+      if let selectedIndexPath = tableView.indexPathForSelectedRow {
+        tableView.deselectRow(at: selectedIndexPath, animated: true)
+      }
+    }
+  }
+  
+  
+  
+  @IBAction func signOutPressed(_ sender: UIBarButtonItem) {
+    do {
+        try authUI!.signOut()
+      print("‼️Sign out Worked")
+      tableView.isHidden = true
+      signIn()
+    } catch {
+      print("Error! Couldn't sign out.")
+    }
+    
+  }
+  
+  
+  
+  
+}
+
+extension SpotsListViewController: UITableViewDelegate, UITableViewDataSource {
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return spots.spotArray.count
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! SpotsTableViewCell
+    cell.nameLabel.text = spots.spotArray[indexPath.row].name
+    return cell
+  }
+  
+  func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    return 60
+  }
+  
+}
+
+extension SpotsListViewController: FUIAuthDelegate {
+  func application(_ app: UIApplication, open url: URL,
+                   options: [UIApplication.OpenURLOptionsKey : Any]) -> Bool {
+    let sourceApplication = options[UIApplication.OpenURLOptionsKey.sourceApplication] as! String?
+    if FUIAuth.defaultAuthUI()?.handleOpen(url, sourceApplication: sourceApplication) ?? false {
+      return true
+    }
+    // other URL handling goes here.
+    return false
+  }
+  
+  func authUI(_ authUI: FUIAuth, didSignInWith user: User?, error: Error?) {
+    // handle user and error as necessary
+    if let user = user {
+      tableView.isHidden = false
+      print("😁 We signed in with user \(user.email ?? "Unknown Email")")
+    }
+  }
+  
+  func authPickerViewController(forAuthUI authUI: FUIAuth) -> FUIAuthPickerViewController {
+    let loginViewController = FUIAuthPickerViewController(authUI: authUI)
+    loginViewController.view.backgroundColor = UIColor.white
+    //setting up the login image
+    let marginInsets: CGFloat = 16
+    let imageHeight: CGFloat = 225
+    let imageY = self.view.center.y - imageHeight
+    let logoFrame = CGRect(x: self.view.frame.origin.x + marginInsets, y: imageY, width: self.view.frame.width - (marginInsets*2), height: imageY)
+    let logoImageView = UIImageView(frame: logoFrame)
+    logoImageView.image = UIImage(named: "logo")
+    logoImageView.contentMode = .scaleAspectFit
+    loginViewController.view.addSubview(logoImageView)
+    return loginViewController
+  }
+  
 }
